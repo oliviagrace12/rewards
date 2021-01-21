c3d5b6fb96f956ea57b1cad00fb6e1fcf8746842
Olivia Chisman <olivia.chisman@gmail.com>
2021-01-20 22:49:49 -0600

    can take profile photo with camera

--- app/src/main/java/com/example/rewards/CreateProfileActivity.java
+++ app/src/main/java/com/example/rewards/CreateProfileActivity.java
@@ -1,20 +1,38 @@
 package com.example.rewards;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.rewards.domain.Profile;
 import com.example.rewards.runnable.CreateProfileAPIRunnable;
 import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class CreateProfileActivity extends AppCompatActivity {
 
@@ -25,7 +43,13 @@ public class CreateProfileActivity extends AppCompatActivity {
     private EditText departmentEditText;
     private EditText titleEditText;
     private EditText storyEditText;
+    private ImageButton imageButton;
     private String apiKey;
+
+    private final int REQUEST_IMAGE_GALLERY = 1;
+    private final int REQUEST_IMAGE_CAPTURE = 2;
+
+    private File currentImageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +69,7 @@ public class CreateProfileActivity extends AppCompatActivity {
         departmentEditText = findViewById(R.id.inputDepartment);
         titleEditText = findViewById(R.id.inputTitle);
         storyEditText = findViewById(R.id.yourStoryInput);
+        imageButton = findViewById(R.id.profileImage);
     }
 
     @Override
@@ -59,6 +84,64 @@ public class CreateProfileActivity extends AppCompatActivity {
             displaySaveDialogue();
         }
         return true;
+    }
+
+    public void selectPhoto(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Profile Picture");
+        builder.setMessage("Take picture from:");
+        builder.setNegativeButton("GALLERY", (dialog, id) -> {});
+        builder.setPositiveButton("CAMERA", (dialog, id) -> doCamera());
+        builder.setNeutralButton("CANCEL", (dialog, id) -> {});
+        builder.setIcon(R.drawable.icon);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void doCamera() {
+        try {
+            currentImageFile = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoURI = FileProvider.getUriForFile(
+                this, "com.example.android.fileprovider", currentImageFile);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            try {
+                processFullCameraImage();
+            } catch (Exception e) {
+                Toast.makeText(this, "onActivityResult: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void processFullCameraImage() {
+
+        Uri selectedImage = Uri.fromFile(currentImageFile);
+        imageButton.setImageURI(selectedImage);
+
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String imageFileName = "image+";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",    /* suffix */
+                storageDir      /* directory */
+        );
     }
 
     private void displaySaveDialogue() {
